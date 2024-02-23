18096738c4d06f9771e1ca7ae4e67fb8dcc2ae29
shanazGit <daizyshan@gmail.com>
2024-02-22 22:33:28 -0500

    change of social media app

---
+++ src/test/java/com/springboot/rta/socialmedia_app/SocialmediaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.rta.socialmedia_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialmediaAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
